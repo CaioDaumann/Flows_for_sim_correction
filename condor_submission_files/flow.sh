4adc50b28ce4cf462cb0caff9ccf7134e1f965eb
Caio Cesar Daumann <daumann@lx3agpu1.physik.rwth-aachen.de>
2023-12-08 14:49:57 +0100

    Added condor submission files

---
+++ condor_submission_files/flow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Activate the pytorch environment
+source /net/software/deep-learning/virtualenv/python3.9/pytorch2.0.1/bin/activate
+
+#Run the training script 
+cd ..
+python main.py
